07ed40a0db561243efe29017e3dfe58bc0ebf32a
SteweQiwi <pashamikus20000@gmail.com>
2022-07-13 13:57:21 +0300

    6 query sets

--- mainproject/autopark/querysets.py
+++ mainproject/autopark/querysets.py
@@ -1,6 +1,6 @@
 from django.utils import timezone
-from mainproject.autopark.models import Order, Refill, Repair, Vehicle
-import datetime
+from mainproject.autopark.models import *
+
 
 
 # QuerySet 1
@@ -10,13 +10,24 @@ q1 = Order.objects.filter(driver__id="1", deadline__gt=timezone.now())
 
 # QuerySet 2
 
-q2 = Refill.objects.filter(created__gt=datetime.datetime.today() - datetime.timedelta(days=30)).values('type', 'price', 'amount')
-# Refill.objects.filter(created__gt=timezone.now()-dateutil.relativedelta(months=1))
+q2 = Refill.objects.filter(created__gt=timezone.now()-timezone.timedelta(days=30), driver__id="1")
 
 
 # QuerySet 3
 
-q3 = Repair.objects.values_list('vehicle__driver__name', flat=True).distinct()
-# q3 = Vehicle.objects.filter(repair_status=2).values_list('driver__name', flat=True).distinct()
+q3 = Driver.objects.filter(vehicle__repair_status=2).distinct()
 
 
+# QuerySet 4
+
+q4 = Driver.objects.filter(order__vehicle=7)
+
+
+# QuerySet 5
+
+q5 = Manager.objects.filter(order__driver=1)
+
+
+# QuerySet 6
+
+q6 = Driver.objects.filter(order__manager=1)
